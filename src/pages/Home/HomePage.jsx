b6f109067bea9af28f47cb6d123d1dc7318c0e1e
Jorge <jrios1991@yahoo.com>
2025-09-23 21:08:33 -0500

    updated featured products component, updated js files to jsx

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { Hero } from "./components/Hero";
+import { FeaturedProducts } from "./components/FeaturedProducts";
 
 export const HomePage = () => {
   return (
     <main>
-      <div>Home</div>
+      <Hero />
+      <FeaturedProducts />
     </main>
   );
 };
