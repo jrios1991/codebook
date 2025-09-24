966208ad74cf9f0efa76a81aea36076cc6ce4d8e
Jorge <jrios1991@yahoo.com>
2025-09-24 00:42:15 -0500

    updated home page with faq and testimonial sections

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { Hero } from "./components/Hero";
 import { FeaturedProducts } from "./components/FeaturedProducts";
+import { Testimonials } from "./components/Testimonial";
+import { Faq } from "./components/Faq";
 
 export const HomePage = () => {
   return (
     <main>
       <Hero />
       <FeaturedProducts />
+      <Testimonials />
+      <Faq />
     </main>
   );
 };
